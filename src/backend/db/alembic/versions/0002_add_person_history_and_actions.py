671a428c50278ab8c4b535570be123da93ae74cf
chun-mingpangext-coder <chun-ming.pang.ext@fashion-digital.de>
2026-08-28 10:14:42 +0200

    feat: implement automatic Alembic migration execution on application startup and rename revision 0002

--- src/backend/db/alembic/versions/0002_add_person_history_and_actions.py
+++ src/backend/db/alembic/versions/0002_add_person_history_and_actions.py
@@ -1,6 +1,6 @@
-"""0002_add_person_history_and_actions
+"""0002_person_history
 
-Revision ID: 0002_add_person_history_and_actions
+Revision ID: 0002_person_history
 Revises: 0001_initial_schema
 Create Date: 2026-08-28 10:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = '0002_add_person_history_and_actions'
+revision: str = '0002_person_history'
 down_revision: Union[str, None] = '0001_initial_schema'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
